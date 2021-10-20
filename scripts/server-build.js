ee6099b7268b1b07ee63c8067c8d7627192eafa2
xiaochu <newi@qq.com>
2021-10-20 10:13:14 +0800

    更新server bin文件

--- scripts/server-build.js
+++ scripts/server-build.js
@@ -3,8 +3,8 @@ process.nextTick(async () => {
   await exec([
     "./scripts/server/index.js",
     "--target",
-    // "win,macos,linux",
-    "host",
+    "win,macos,linux",
+    // "host",
     "--output",
     "bin/server",
   ]);
