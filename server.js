bb2544df9fc7a58c8c989e916e41f941273a4631
xiaochu <newi@qq.com>
2022-03-01 11:04:07 +0800

    修复 JSON POST 请求挂起问题

--- server.js
+++ server.js
@@ -28,6 +28,58 @@ function morkBuild(obj, req) {
   }
   return obj;
 }
+function htmlRequest(setting) {
+  return function (req, res, next) {
+    if (req.accepts("html")) {
+      res.contentType = "text/html";
+      for (const filename of setting.defaultIndex) {
+        const filePath = path.join(setting.html, filename);
+        if (fs.existsSync(filePath)) {
+          // const fileBody = fs.readFileSync(filePath, { encoding: "utf-8" });
+          if (setting.toIndex) {
+            res.sendFile(filePath);
+            return;
+          }
+        }
+      }
+      res.send("Not Found");
+      return;
+    }
+    next();
+  };
+}
+function mockRequest(morkRule) {
+  return function (req, res) {
+    const { _meta = {}, ...morkRes } = morkBuild(morkRule.body, req);
+    const meta = Object.assign(
+      { statusCode: 200, headers: {}, cookies: {} },
+      _meta
+    );
+    // 设置响应状态码
+    res.statusCode = meta.statusCode;
+    // 设置header参数
+    Object.entries(meta.headers).forEach(([key, val]) => {
+      res.setHeader(key, val);
+    });
+    // 设置cookie参数
+    Object.entries(meta.cookies).forEach(([key, val]) => {
+      res.cookie(key, val);
+    });
+    res.send(Mock.mock(morkRes));
+  };
+}
+// 用于处理JSON数据
+function onProxyReq(proxyReq, req, res) {
+  // add custom header to request
+  if (req.body) {
+    let bodyData = JSON.stringify(req.body);
+    // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+    proxyReq.setHeader("Content-Type", "application/json");
+    proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+    // stream the content
+    proxyReq.write(bodyData);
+  }
+}
 
 function main(setting) {
   const app = express();
@@ -41,6 +93,12 @@ function main(setting) {
   }
   //此处设置配置文件的路径
   if (setting) {
+    const htmlUrl =
+      setting.html && fs.existsSync(setting.html) ? setting.html : false;
+    let isMatchHtml = false;
+    const htmlMiddleware = htmlUrl
+      ? [serveIndex(setting.html, { icons: true }), htmlRequest(setting)]
+      : [];
     // mock 配置
     if (setting.mock) {
       const mockConfig = Object.entries(setting.mock);
@@ -49,28 +107,11 @@ function main(setting) {
         for (const morkRule of morkRules) {
           const method = morkRule.method.toLowerCase();
           if (morkRule.on && typeof router[method] === "function") {
-            router[method](morkRule.url, function (req, res) {
-              const { _meta = {}, ...morkRes } = morkBuild(morkRule.body, req);
-              const meta = Object.assign(
-                { statusCode: 200, headers: {}, cookies: {} },
-                _meta
-              );
-              // 设置响应状态码
-              res.statusCode = meta.statusCode;
-              // 设置header参数
-              Object.entries(meta.headers).forEach(([key, val]) => {
-                res.setHeader(key, val);
-              });
-              // 设置cookie参数
-              Object.entries(meta.cookies).forEach(([key, val]) => {
-                res.cookie(key, val);
-              });
-              res.send(Mock.mock(morkRes));
-            });
+            router[method](morkRule.url, mockRequest(morkRule));
           }
         }
         console.log("[MOCK]", mockBaseUrl);
-        app.use(mockBaseUrl, upload.array(), [
+        const mockMiddleware = [
           router,
           function (req, res) {
             if (req.accepts("json")) {
@@ -82,39 +123,42 @@ function main(setting) {
               return;
             }
           },
-        ]);
+        ];
+        if (mockBaseUrl === htmlUrl) {
+          mockMiddleware.push(...htmlMiddleware);
+          isMatchHtml = true;
+        }
+        app.use(mockBaseUrl, upload.array(), mockMiddleware);
       }
     }
     // proxy 配置
     if (setting.proxy) {
       const proxyConfig = Object.entries(setting.proxy);
       for (const [proxyBaseUrl, proxyOption] of proxyConfig) {
-        app.use(proxyBaseUrl, createProxyMiddleware(proxyOption));
+        // 修复 JSON POST 请求挂起问题
+        if (!proxyOption.onProxyReq) {
+          proxyOption.onProxyReq = onProxyReq;
+        } else {
+          const proxyReqFn = proxyOption.onProxyReq;
+          proxyOption.onProxyReq = (...args) => {
+            onProxyReq(...args);
+            proxyReqFn(...args);
+          };
+        }
+        const proxyItem = createProxyMiddleware(proxyOption);
+        if (proxyBaseUrl === htmlUrl) {
+          isMatchHtml = true;
+          app.use(proxyBaseUrl, [proxyItem, ...htmlMiddleware]);
+        } else {
+          app.use(proxyBaseUrl, proxyItem);
+        }
       }
     }
     // html 配置
-    if (setting.html && fs.existsSync(setting.html)) {
+    if (htmlUrl && !isMatchHtml) {
       app.use(express.static(setting.html, {}));
       //需要配置在 proxy配置后面
-      app.use(setting.htmlUrl, [
-        serveIndex(setting.html, { icons: true }),
-        function (req, res) {
-          if (req.accepts("html")) {
-            res.contentType = "text/html";
-            for (const filename of setting.defaultIndex) {
-              const filePath = path.join(setting.html, filename);
-              if (fs.existsSync(filePath)) {
-                // const fileBody = fs.readFileSync(filePath, { encoding: "utf-8" });
-                if (setting.toIndex) {
-                  res.sendFile(filePath);
-                  return;
-                }
-              }
-            }
-            res.send("Not Found");
-          }
-        },
-      ]);
+      app.use(setting.htmlUrl, htmlMiddleware);
     }
     app.listen(setting.port, setting.host, function () {
       console.log("服务启动成功，端口为:" + setting.port);
